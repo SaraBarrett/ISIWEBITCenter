1258df21a85eff44a1d1fb20f8033011c222a65d
SaraBarrett <sara.c.mon@gmail.com>
2023-11-06 15:40:26 +0000

    master

--- resources/views/general/home.blade.php
+++ resources/views/general/home.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cheguei a casa</title>
-</head>
-
-<body>
+@section('content')
     <h2>CHeguei a casa</h2>
 
     <ul>
@@ -20,6 +12,4 @@
         </a>
     </ul>
     {{ $hello }}
-</body>
-
-</html>
+@endsection
